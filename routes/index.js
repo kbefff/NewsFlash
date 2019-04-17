0c82a5bf642f01db3c22b6517db0cac04232dc77
kbefff <kbeffel10@me.com>
2019-04-16 17:38:04 -0700

    home and saved views

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+var router = require('express').Router;
+var apiRoutes = require('./api');
+var viewRoutes = require('./view');
+
+router.use('/api', apiRoutes);
+router.use('/', viewRoutes);
---
+++ routes/view/index.js
@@ -0,0 +1,27 @@
+var router = require('express').Router;
+var db = require("../../models");
+
+//this will render the homepage
+router.get('/', function(req, res){
+    db.Headline.find({saved: false})
+        .sort({date: -1})
+        .then(function(dbArticles){
+            // res.render takes the name of the file we want to render (this will be home.handlebars)
+            // then inject articles into it
+            // the articles are coming from dbArticles
+            res.render('home', {articles: dbArticles})
+        })
+});
+
+router.get('/saved', function(req, res){
+    db.Headline.find({saved: true})
+        .sort({date: -1})
+        .then(function(dbArticles){
+            // res.render takes the name of the file we want to render (this will be savved.handlebars)
+            // then inject articles into it
+            // the articles are coming from dbArticles
+            res.render('saved', {articles: dbArticles})
+        })
+});
+
+module.exports = router;
